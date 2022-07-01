26dc3d98b09bfcb5e4356a73b3d241bc503a5f83
matthew p robertson <35159349+mpr1255@users.noreply.github.com>
2022-07-01 21:51:29 +1000

    testing

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "net/http"
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
 func main() {
-	println("hello!")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello!"))
 }
